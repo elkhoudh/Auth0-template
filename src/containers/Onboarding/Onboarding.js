99d4555d2f4b479c9cea058488eab96b20c75021
Hamza Elkhoudiri <elkhoudh@gmail.com>
2019-07-15 19:50:00 -0400

    Testing sentry

--- src/containers/Onboarding/Onboarding.js
+++ src/containers/Onboarding/Onboarding.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Onborading = props => {
-  return <h1>Onborading</h1>;
+  return <h1 onClick={tits}>Onborading</h1>;
 };
 
 export default Onborading;
--- src/index.js
+++ src/index.js
@@ -4,8 +4,13 @@ import { Router, withRouter } from "react-router-dom";
 import history from "./containers/auth-zero/history";
 import "./index.css";
 import App from "./App";
+import * as Sentry from "@sentry/browser";
 
 const AppWithRouter = withRouter(App);
+
+Sentry.init({
+  dsn: "https://21869e247bc34426948c638aed297a3a@sentry.io/1505550"
+});
 
 ReactDOM.render(
   <Router history={history}>
